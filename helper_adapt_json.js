cbef0e7bdb9bd89aee2488c99f4e206b73933fb6
Simon Walz <contact@simonwalz.de>
2021-01-17 21:25:37 +0100

    +templates, +script adaption

--- helper_adapt_json.js
+++ helper_adapt_json.js
@@ -1,19 +1,41 @@
 const fs = require("fs");
 
+var merge = function(a, b) {
+	var changed = false;
+	Object.keys(b).forEach(function(key) {
+		if (typeof b[key] === "object" && b[key] !== null &&
+				typeof a[key] === "object" && a[key] !== null) {
+			changed = merge(a[key], b[key]) || changed;
+			return;
+		}
+		if (a[key] !== b[key]) {
+			a[key] = b[key];
+			changed = true;
+		}
+	});
+
+	return changed;
+};
+
 exports.adapt_json = function(filename, object, options) {
 	try {
 		var schema = JSON.parse(fs.readFileSync(filename));
 		// throw file not found
 		// throw json not valid
 
-		if (typeof object === "function") {
-			schema = object(schema, filename);
-		} else if (typeof object === "object") {
-			// TODO: Merge object
-			Object.keys(object).forEach(function(key) {
-				schema[key] = object[key];
-			});
+		if (!Array.isArray(object)) {
+			object = [object];
 		}
+		var changed = false;
+		object.forEach(function(o) {
+			if (typeof o === "function") {
+				changed = o(schema, filename) || changed;
+			} else if (typeof o === "object") {
+				changed = merge(schema, o) || changed;
+			}
+		});
+		if (!changed) return false;
+
 		var content = JSON.stringify(schema, null,
 			(options && options.spaces ? options.spaces : "\t"));
 		if (options && options.simulate) {
@@ -22,8 +44,10 @@ exports.adapt_json = function(filename, object, options) {
 			return;
 		}
 
-		fs.writeFileSync(filename, content);
+		fs.writeFileSync(filename, content+"\n");
+		return true;
 	} catch (err) {
 		console.error(err);
 	}
+	return false;
 };
--- template/index.js
+++ template/index.js
@@ -1,3 +1,7 @@
 exports.init = function(node, app_config, main) {
+	node.announce([{
+		"type": "MYAPP.app"
+	}, app_config.metadata]);
 
+	return [node];
 };
